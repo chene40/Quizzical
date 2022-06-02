6ecf02a0fcc6e7b2ae6210e4a48ffd615f7e27b2
Eric <chene40@mcmaster.ca>
2022-06-01 21:44:18 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import StartPage from './components/StartPage'
+
+export default function App(){
+  return(
+    <div>
+      {/* https://opentdb.com/api_config.php */}
+      {/* https://www.figma.com/file/boFdwjzOLUzxmgWCzCRvdT/Quizzical-App-(Copy)?node-id=8%3A2 */}
+      <StartPage />
+    </div>
+  )
+}
---
+++ src/components/StartPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function StartPage(){
+
+    return(
+        <div className="start-page">
+            <h1 className="start-page-title">Quizzical</h1>
+            <p className="start-page-desc">Some description if needed</p>
+            <button className="start-page-button">Start quiz</button>
+        </div>
+    )
+}
